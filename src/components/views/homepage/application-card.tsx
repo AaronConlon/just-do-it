58e04f085576679d6b8851a1873491966d6fdc7d
Developer27149 <564151947@qq.com>
2025-01-23 16:40:10 +0800

    feat: application card

--- src/components/views/homepage/application-card.tsx
+++ src/components/views/homepage/application-card.tsx
@@ -1,11 +1,10 @@
 import { Badge } from '@/components/ui/badge'
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
+import { Rating } from '@/components/ui/raiting'
 import { TApp } from '@/lib/schemas'
-import { Rating } from '@smastrom/react-rating'
 import Image from 'next/image'
 import Link from 'next/link'
-
-import '@smastrom/react-rating/style.css'
+import { PiUserThin } from 'react-icons/pi'
 
 interface ApplicationCardProps {
   application: TApp
@@ -50,12 +49,17 @@ export function ApplicationCard({ application }: ApplicationCardProps) {
           </div>
           {/* user count and rank */}
           <div className="mt-4 flex items-center justify-between">
-            <div className="flex items-center gap-2">
-              <Image src="/logo.svg" alt="user" width={20} height={20} />
-              <span>{application.user_count}</span>
-            </div>
+            {application.user_count ? (
+              <div className="flex items-center gap-2">
+                <PiUserThin size={20} />
+                <span>{application.user_count}</span>
+              </div>
+            ) : (
+              'No users yet'
+            )}
+
+            <Rating size="sm" max={5} value={application.rank} className="text-orange-400" />
           </div>
-          <Rating style={{ maxWidth: 5 }} value={application.rank} readOnly />
         </CardContent>
       </Card>
     </Link>
--- src/components/views/homepage/basic-description.tsx
+++ src/components/views/homepage/basic-description.tsx
@@ -1,26 +1,48 @@
+import BlurText from '@/components/common/BlurText'
 import { SocialLinks } from '@/components/common/SocialLinks'
+import SquaresComponent from '@/components/common/Squares/index'
 import Image from 'next/image'
 
 export default function BasicDescription() {
   return (
-    <div className="bg-gradient-to-tr from-white via-slate-50 to-gray-50 p-4">
-      <div className="my-4 p-8 text-center text-5xl font-semibold">Build something amazing.</div>
-      <div className="flex flex-wrap gap-4">
-        <Image
-          src="https://de4965e.webp.li/blog-images/2025/01/3fbc98b659f8bc840fc6af6469ed5c85.jpg"
-          alt="avatar cover"
-          width={400}
-          height={400}
-          className="rounded-lg"
+    <div>
+      <div className="relative mx-4 h-[300px] overflow-hidden rounded-lg bg-gray-900">
+        <div className="absolute left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%] transform text-center text-white">
+          <BlurText
+            text="Build something amazing."
+            delay={150}
+            animateBy="words"
+            direction="top"
+            className="mb-8 text-5xl"
+          />
+        </div>
+        <SquaresComponent
+          speed={0.3}
+          squareSize={40}
+          direction="diagonal" // up, down, left, right, diagonal
+          borderColor="#eeeeee11"
+          hoverFillColor="#222222"
         />
-        <div>
-          <p className="w-[640px] max-w-[90%] indent-8 sm:p-4">
-            {`Hi there! 👋 I'm a passionate Web Developer who loves building useful applications and
+      </div>
+
+      <div className="bg-gradient-to-tr from-white via-slate-50 to-gray-50 p-4">
+        <div className="flex flex-wrap gap-4">
+          <Image
+            src="https://de4965e.webp.li/blog-images/2025/01/3fbc98b659f8bc840fc6af6469ed5c85.jpg"
+            alt="avatar cover"
+            width={400}
+            height={400}
+            className="rounded-lg"
+          />
+          <div>
+            <p className="w-[640px] max-w-[90%] indent-8 sm:p-4">
+              {`Hi there! 👋 I'm a passionate Web Developer who loves building useful applications and
         services. This is my digital garden where I showcase all the projects I've created. From web
         apps to backend services and innovative products - you'll find everything I've built right
         here. Feel free to explore my work and reach out if you'd like to collaborate!`}
-          </p>
-          <SocialLinks />
+            </p>
+            <SocialLinks />
+          </div>
         </div>
       </div>
     </div>
